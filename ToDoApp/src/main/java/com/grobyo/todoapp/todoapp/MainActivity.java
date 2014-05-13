bb27b7d65ab2e28f103dd6a842a46d2e902d07d1
andy Kettle <andy@grobyo.com>
2014-05-13 16:56:58 -0700

    Andy Commit 5/13

--- ToDoApp/src/main/java/com/grobyo/todoapp/todoapp/MainActivity.java
+++ ToDoApp/src/main/java/com/grobyo/todoapp/todoapp/MainActivity.java
@@ -16,7 +16,6 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 
-
 public class MainActivity extends ActionBarActivity {
     ArrayList<String> items;
     ArrayAdapter<String> itemsAdapter;
